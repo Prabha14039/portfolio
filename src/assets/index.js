4b9c715c86560724d7549393b01256a256e5f91c
abhi <138896016+Prabha14039@users.noreply.github.com>
2024-06-20 20:21:59 +0530

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -7,19 +7,18 @@ import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
 
-import css from "./tech/css.png";
-import docker from "./tech/docker.png";
-import figma from "./tech/figma.png";
+import Python from "./tech/python.png";
 import git from "./tech/git.png";
-import html from "./tech/html.png";
 import javascript from "./tech/javascript.png";
-import mongodb from "./tech/mongodb.png";
+import postgres from "./tech/Postgres.png";
 import nodejs from "./tech/nodejs.png";
-import reactjs from "./tech/reactjs.png";
-import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
-import typescript from "./tech/typescript.png";
+import C from "./tech/C.png";
 import threejs from "./tech/threejs.svg";
+import c++ from "./tech/c++.svg";
+import dart from "./tech/dart.svg";
+import pytorch from "./tech/pytorch.svg";
+import tensorflow from "./tech/tensorflow.svg";
 
 import meta from "./company/meta.png";
 import shopify from "./company/shopify.png";
@@ -39,19 +38,18 @@ export {
   github,
   menu,
   close,
-  css,
-  docker,
-  figma,
+  python,
   git,
-  html,
   javascript,
-  mongodb,
+  postgres,
   nodejs,
-  reactjs,
-  redux,
   tailwind,
-  typescript,
+  C,
   threejs,
+  c++,
+  dart,
+  pytorch,
+  tensorflow,
   meta,
   shopify,
   starbucks,
