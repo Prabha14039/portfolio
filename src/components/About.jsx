f6d7d0e1f1791bd3e4409064fb557e9d72ee2338
abhi <138896016+Prabha14039@users.noreply.github.com>
2024-06-20 20:28:55 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -45,7 +45,7 @@ const About = () => {
         variants={fadeIn("","",0.1,1)}
     className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
         >
-        I'm a skilled developers
+        I'm a skilled developer
       </motion.p>
     <div className="mt-20 flex flex-wrap gap-10">
       {services.map((service,index)=>(
